b2913fd0bd05d5cff50e6f749ca449a21233bc2a
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-04-13 01:06:01 +0530

    dev env redy

--- backend/config/production.js
+++ backend/config/production.js
@@ -18,19 +18,19 @@ config.server.port = 5001
 /******************** Blockchain *************************/
 config.blockchain = {}
 config.blockchain.url = "https://sepolia.infura.io/v3/dd0d1ffd15954086a4c1eed15171a62d"
-config.blockchain.chainId = "5"
+config.blockchain.chainId = "11155111"
 /******************** Blockchain *************************/
 
 
 /******************** Smart Contract *************************/
 config.smartContract = {}
 config.smartContract.upgradToken = {}
-config.smartContract.upgradToken.address = "0x24ACb50C27CD21D05a9302686b1256d8a081602e" // need to upgrad:done
+config.smartContract.upgradToken.address = "0x3e4202D63e5d3EE64a1fC1A1eB3504646065Ae6A" // need to upgrad:done
 config.smartContract.upgradToken.gasLimit = 100000
-config.smartContract.upgradToken.buildPath = smartContractDirectory + "/artifacts/contracts/CatTokenContract.sol/UpgradToken.json"
+config.smartContract.upgradToken.buildPath = smartContractDirectory + "/artifacts/contracts/UpgradTokenContract.sol/UpgradToken.json"
 config.smartContract.defiplatform = {}
-config.smartContract.defiplatform.address = "0x45E6aB80EFFaAcd8d430B696452443De110FC0B9" // need to upgrad:done
-config.smartContract.defiplatform.gasLimit = 6000000
+config.smartContract.defiplatform.address = "0x7F371DfFE9Fa44435900be27059B62dae4CA80b1" // need to upgrad:done
+config.smartContract.defiplatform.gasLimit = 2000000
 config.smartContract.defiplatform.buildPath = smartContractDirectory + "/artifacts/contracts/DeFiPlatformContract.sol/DefiPlatform.json"
 /******************** Smart Contract *************************/
 
