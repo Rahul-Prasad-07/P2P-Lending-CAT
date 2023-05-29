5a0637c467c0dff71be892fadc07030c5c5012ff
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-05-29 17:50:50 +0530

    Add all

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:5001/v1'
+  apiUrl: 'http://127.0.0.1:5001'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:5001/v1'
+  apiUrl: 'http://127.0.0.1:5001'
   // apiUrl: 'https://6472e1ba56504475fdbef4c5--bespoke-choux-6a3fbe.netlify.app/'
 };
 
