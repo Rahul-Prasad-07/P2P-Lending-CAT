3f6df121e742d29a6d68bdb73fff9cf898cff5bf
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-06-05 18:13:14 +0530

    chnage env

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,6 +6,6 @@ const prodConfig = require('./production.js');
 if (process.env["ENV"] === 'development') { module.exports = devConfig; }
 else if (process.env["ENV"] === 'staging') { module.exports = stagConfig; }
 else if (process.env["ENV"] === 'production') { module.exports = prodConfig; }
-else module.exports = devConfig;
+else module.exports = prodConfig;
 
 module.exports = devConfig; 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:5001/v1'
+  apiUrl: 'http://127.0.0.1:5001/V1'
 };
 
 /*
